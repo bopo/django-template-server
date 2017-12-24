7475bc05bdc56a38254c331e9acc5e9ae71f9176
bopo <ibopo@126.com>
2017-12-24 10:36:24 +0800

    up`

--- service/customer/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-default_app_config = 'service.customer.ConsumerConfig'
-from django.apps import AppConfig
-from django.utils.translation import ugettext_lazy as _
-
-
-class ConsumerConfig(AppConfig):
-    name = 'service.customer'
-    verbose_name = _(u'用户管理')
